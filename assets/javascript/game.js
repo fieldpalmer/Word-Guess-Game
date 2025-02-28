392d35efeebc88445a45b172187c1a13bda44014
Field Palmer <gfieldpalmer@gmail.com>
2025-02-27 19:59:47 -0700

    refactor complete for now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,147 @@
-function game () {
-    
-}
+class HangmanGame {
+   constructor(vocab) {
+      this.vocab = vocab;
+      this.answer = '';
+      this.answerLetters = [];
+      this.answerBlanks = [];
+      this.wrongLetters = [];
+      this.guessesLeft = 10; // Matches hangman parts count
+      this.rightGuessCount = 0;
+      this.winCount = 0;
+      this.lossCount = 0;
+      this.wordsPlayed = [];
+      this.hangmanParts = document.querySelectorAll('.hangman-part');
 
-var vocab = ["alchemy","chemistry","alcohol","algebra","algorithm","alkaline","almanac","average","azimuth","cipher","nadir","soda","zenith","admiral","adobe","alcove","amber","arsenal","assassin","mafia","caliber","candy","check","cork","coffee","cotton","gauze","guitar","hazard","lazuli","mascara","mattress","monsoon","racquet","ream","safari","sash","satin","sofa","talcum","tariff","magazine","cover",];
-var answer = vocab[Math.floor(Math.random() * vocab.length)];
-var wordsPlayed = [];
-var wordsPlayedBox = document.getElementById("words-played");
+      this.lettersBox = document.getElementById('letters-box');
+      this.keyInputBox = document.getElementById('key-input');
+      this.guessesLeftText = document.getElementById('guesses-left');
+      this.winCountText = document.getElementById('win-count');
+      this.lossCountText = document.getElementById('loss-count');
+      this.wordsPlayedBox = document.getElementById('words-played');
 
-var answerBlanks = [];
-for ( var i = 0 ; i < answer.length ; i++) {
-    answerBlanks.push("_");
-}
+      this.initializeGame();
+   }
 
-var answerBlanksText = document.getElementById("letters-box");
-answerBlanksText.innerHTML =  "Mystery Word: " + answerBlanks.join(" ");
+   initializeGame() {
+      this.reset();
+      document.addEventListener('keyup', (event) => this.handleKeyPress(event));
+   }
 
-var answerLetters = answer.split("");
-var wrongLetters = [] ;
-var wrongLettersText = document.getElementById("key-input");
-var guessesLeft = 10;
-var guessesLeftText = document.getElementById("guesses-left");
-var rightGuessCount = 0;
-var winCount = 0;
-var winCountText = document.getElementById("win-count");
-var lossCount = 0;
-var lossCountText = document.getElementById("loss-count");
+   reset() {
+      this.guessesLeft = 10;
+      this.rightGuessCount = 0;
+      this.wrongLetters = [];
+      this.answer = this.vocab[Math.floor(Math.random() * this.vocab.length)];
+      this.answerLetters = this.answer.split('');
+      this.answerBlanks = Array(this.answer.length).fill('_');
 
-function reset() {
+      // Hide all hangman parts
+      this.hangmanParts.forEach((part) => part.classList.remove('show'));
 
-    userLetter = 0 ;
-    rightGuessCount = 0;
-    guessesLeft = 10;
-    guessesLeftText.innerHTML = guessesLeft;
-    wrongLetters = [];
-    answerBlanks = [];
-    answer = vocab[Math.floor(Math.random() * vocab.length)];
-    answerLetters = answer.split("");
-    
-    answerBlanks = [];
-    for (var i = 0 ; i < answer.length ; i++) {
-        answerBlanks.push("_")
-    }
+      this.updateUI();
+   }
 
-    answerBlanksText.innerHTML = "Mystery Word: " + answerBlanks.join(" ");
-    wrongLettersText.innerHTML = wrongLetters;
+   updateUI() {
+      this.lettersBox.innerHTML = 'Mystery Word: ' + this.answerBlanks.join(' ');
+      this.keyInputBox.innerHTML = this.wrongLetters.join(' ');
+      this.guessesLeftText.innerHTML = this.guessesLeft;
+      this.winCountText.innerHTML = this.winCount;
+      this.lossCountText.innerHTML = this.lossCount;
+      this.wordsPlayedBox.innerHTML = this.wordsPlayed.join(' ');
+   }
 
-}
+   handleKeyPress(event) {
+      let userLetter = event.key.toLowerCase();
 
-document.onkeyup = function(event) {
+      if (
+         !/[a-z]/.test(userLetter) ||
+         this.wrongLetters.includes(userLetter) ||
+         this.answerBlanks.includes(userLetter)
+      ) {
+         return;
+      }
 
-    answerBlanksText.innerHTML =  "Mystery Word: " + answerBlanks.join(" ");
-
-    var userLetter = event.key;
-
-    if (answer.indexOf(userLetter) > -1) {
-        for ( var j = 0 ; j < answer.length ; j++ ) {
-            if (answerLetters[j] === userLetter) {
-                answerBlanks[j] = userLetter;
-                rightGuessCount++;
-                answerBlanksText.innerHTML = "Mystery Word: " + answerBlanks.join(" ");
+      if (this.answer.includes(userLetter)) {
+         this.answerLetters.forEach((letter, index) => {
+            if (letter === userLetter) {
+               this.answerBlanks[index] = userLetter;
+               this.rightGuessCount++;
             }
-        }
-    } else {
-        wrongLetters.push(userLetter);
-        guessesLeft--;
-        wrongLettersText.innerHTML = wrongLetters.join(" ");
-        guessesLeftText.innerHTML = guessesLeft;
-    } 
-    
-    function winLose () {
-        if (rightGuessCount === answer.length) {
-            winCount++;
-            winCountText.innerHTML = winCount;  
-            wordsPlayed.push(answer);
-            alert("You won!");
-            wordsPlayedBox.innerHTML = wordsPlayed.join(" ");
-            reset(); 
-        } 
-        if (guessesLeft === 0) {
-            lossCount++;
-            lossCountText.innerHTML = lossCount;
-            wordsPlayed.push(answer);
-            alert("You lose!");
-            wordsPlayedBox.innerHTML = wordsPlayed.join(" ");
-            reset();
-        }
-    }
+         });
+      } else {
+         this.wrongLetters.push(userLetter);
+         this.guessesLeft--;
 
-    winLose();
-};
+         // Show next hangman part
+         let wrongIndex = 10 - this.guessesLeft - 1;
+         if (this.hangmanParts[wrongIndex]) {
+            this.hangmanParts[wrongIndex].classList.add('show');
+         }
+      }
+
+      this.checkWinLoss();
+      this.updateUI();
+   }
+
+   checkWinLoss() {
+      if (this.rightGuessCount === this.answer.length) {
+         this.winCount++;
+         this.wordsPlayed.push(this.answer);
+         alert('You won!');
+         this.reset();
+      } else if (this.guessesLeft === 0) {
+         this.lossCount++;
+         this.wordsPlayed.push(this.answer);
+         alert('You lost! The word was: ' + this.answer);
+         this.reset();
+      }
+   }
+}
+
+const vocab = [
+   'alchemy',
+   'chemistry',
+   'alcohol',
+   'algebra',
+   'algorithm',
+   'alkaline',
+   'almanac',
+   'average',
+   'azimuth',
+   'cipher',
+   'nadir',
+   'soda',
+   'zenith',
+   'admiral',
+   'adobe',
+   'alcove',
+   'amber',
+   'arsenal',
+   'assassin',
+   'mafia',
+   'caliber',
+   'candy',
+   'check',
+   'cork',
+   'coffee',
+   'cotton',
+   'gauze',
+   'guitar',
+   'hazard',
+   'lazuli',
+   'mascara',
+   'mattress',
+   'monsoon',
+   'racquet',
+   'ream',
+   'safari',
+   'sash',
+   'satin',
+   'sofa',
+   'talcum',
+   'tariff',
+   'magazine',
+   'cover'
+];
+
+const hangman = new HangmanGame(vocab);
